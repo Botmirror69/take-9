a171683ef2aaefab8c8d41053d689728c0b775a9
Botmirror69 <92679061+Botmirror69@users.noreply.github.com>
2022-07-17 10:13:53 +0530

    Add files via upload

--- api/index.php
+++ api/index.php
@@ -2,27 +2,39 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="description" content="ENJOY FREE LIVE JIOTV">
+    <meta name="keywords" content="JIOTV, LIVETV, SPORTS, MOVIES, MUSIC">
+    <meta name="author" content="Techie Sneh">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no" />
-    <title>JIOTV 2.0</title>
+    <title>JIOTV PREMIUM</title>
     <link rel="shortcut icon" type="image/x-icon" href="https://i.ibb.co/37fVLxB/f4027915ec9335046755d489a14472f2.png">
     <meta name="robots" content="noindex" />
     <link rel="stylesheet" href="assets/css/techiesneh.min.css">
-    <link rel="stylesheet" href="assets/search.css">
+    <link rel="stylesheet" href="assets/css/search.css">
+    <!-- Player CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/plyr@3.6.2/dist/plyr.css" />
+    <script src="https://cdn.jsdelivr.net/npm/plyr@3.6.12/dist/plyr.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/hls.js@1.1.4/dist/hls.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+    <!-- Main JS -->
     <script src="https://cdn.jsdelivr.net/npm/lazysizes@5.3.2/lazysizes.min.js"></script>
     <script src="https://code.iconify.design/2/2.1.2/iconify.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.5/lazysizes.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
 </head>
 
 <body>
     <div id="jtvh1">
-        <a href="https://www.jio.com/en-in/apps/jio-tv">
-            <h1>JIOTV 2.0</h1>
+        <a href="">
+            <h1>JIOTV PREMIUM</h1>
+        </a>
     </div>
-    </a>
     <div id="searchWrapper">
         <input type="text" name="searchBar" id="searchBar" placeholder="Search ..." />
     </div>
@@ -30,11 +42,22 @@
     <div id="content">
         <div class="container">
             <div id="charactersList" class="row">
-
             </div>
         </div>
     </div>
-    <script src="assets/search1.js"></script>
+    <div class="modal fade" id="watchTV" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="ratio ratio-16x9">
+                        <video autoplay crossorigin playsinline controls id="vid"></video>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="assets/player.js"></script>
+    <script src="assets/search.js"></script>
 </body>
 
 </html>
